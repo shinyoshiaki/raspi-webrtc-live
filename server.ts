2eecbe103674a5f5c2caf6495e470b75beddd486
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-05-03 17:59:54 +0900

    raspi

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const udp = createSocket("udp4");
 udp.bind(5000);
 
 const child = exec(
-  "gst-launch-1.0 videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
+  "gst-launch-1.0 -v -e v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480 ! vp8enc error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true cpu-used=5 deadline=1 ! rtpvp8pay ! udpsink host=127.0.0.1 port=5000"
 );
 
 server.on("connection", async (socket) => {
